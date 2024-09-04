061933596043b8d3faee2582d27337c35a036c43
Isaac Davi <izaacddavid98@gmail.com>
2024-09-04 15:42:59 -0300

    Adicionando um exemplo de array que será usado para o jogo da Forca

--- 2. Manipulando Arrays/Forca.c
+++ 2. Manipulando Arrays/Forca.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    // char letra1 = 'M';    char letra2 = 'E';    char letra3 = 'L';    char letra4 = 'A';    char letra5 = 'O';
+    //printf("%c%c%c%c%c", letra1, letra2, letra3, letra4, letra5);
+    // A fim de não fazer assim todas as vezes, usaremos de um Array para guardar a palavra.
+    
+    int notas[10];
+    notas[0] = 10;
+    notas[2] = 9;
+    notas[9] = 4;
 
+    printf("Notas %d - %d - %d", notas[0], notas[2], notas[9]);
 }
