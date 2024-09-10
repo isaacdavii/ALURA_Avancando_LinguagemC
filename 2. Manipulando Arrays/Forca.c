3335737ddb10327e99e9f86fb3c5c2b5276169a2
Isaac Davi <izaacddavid98@gmail.com>
2024-09-10 12:13:55 -0300

    Criamos função para desenhar forca e escolher a palavra no nosso jogo.

--- 2. Manipulando Arrays/Forca.c
+++ 2. Manipulando Arrays/Forca.c
@@ -28,6 +28,24 @@ int jaChutou (char letra, char chutes[26], int tentativas) {
     return achou;
 }
 
+void desenhaForca(char palavraSecreta[20], char chutes[26], int tentativas) {
+    for (int i = 0; i < strlen(palavraSecreta); i++) {
+        
+        int achou = jaChutou(palavraSecreta[i], chutes, tentativas);
+
+        if (achou) {
+            printf("%c ", palavraSecreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhePalavra(char palavraSecreta[20]) {
+    sprintf(palavraSecreta, "MELANCIA");
+}
+
 int main() {
     /* 1. Inicialiando uma palavra
     char letra1 = 'M';    char letra2 = 'E';    char letra3 = 'L';    char letra4 = 'A';    char letra5 = 'O';
@@ -55,8 +73,10 @@ int main() {
 
     char palavraSecreta[20];
 
-    sprintf(palavraSecreta, "MELANCIA");
-    // printf("%s", palavraSecreta);
+    /* 9. Escolhendo a palavra secreta
+        sprintf(palavraSecreta, "MELANCIA");
+        printf("%s", palavraSecreta); // %s para string
+    */
 
     int acertou = 0;
     int enforcou = 0;
@@ -74,17 +94,18 @@ int main() {
 
     do {
 
+        /* 8. Imprime a palavra secreta
         for (int i = 0; i < strlen(palavraSecreta); i++) {
 
-            /* 7. Verificando se a letra já foi chutada ou não
-            for (int j = 0; j < tentativas; j++) { // Percorre todas as tentativas feitas até o momento
-                if (chutes[j] == palavraSecreta[i]) { // Percorre todas as letras da palavra secreta
-                    achou = 1; 
-                    break;
-                }
-            }
-            */
-           int achou = jaChutou(palavraSecreta[i], chutes, tentativas);
+            // 7. Verificando se a letra já foi chutada ou não
+            //for (int j = 0; j < tentativas; j++) { // Percorre todas as tentativas feitas até o momento
+            //    if (chutes[j] == palavraSecreta[i]) { // Percorre todas as letras da palavra secreta
+            //        achou = 1; 
+            //       break;
+            //    }
+            //}
+            
+            int achou = jaChutou(palavraSecreta[i], chutes, tentativas);
 
             if (achou) {
                 printf("%c ", palavraSecreta[i]);
@@ -94,7 +115,10 @@ int main() {
             
         }
         printf("\n");
-        
+        */
+
+        desenhaForca(palavraSecreta, chutes, tentativas);
+
         /* 4. Verificando se o chute está na palavra
         for (int i = 0; i < strlen(palavraSecreta); i++) { // Loop para varrer o Array verificando compatibilidade
             if (palavraSecreta[i] == chute ) {
@@ -113,7 +137,6 @@ int main() {
         */
         
         chuta(chutes, &tentativas); // Passamos o endereço em tentativas para se ler na função
-        
 
     } while(!acertou && enforcou == 0); // Usar a exclamação para negar a condição é igual o '== 0'. Usamos isso porque as variáveis são boolenas.
 
